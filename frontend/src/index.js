c6cd919c306079b3d6f748409b083a2e6335db2a
Delta student <sachin2005mishra@gmail.com>
2025-12-20 00:55:38 +0530

    About section is completed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import './index.css';
 import HomePage from './landing_page/Home/HomePage';
+
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import PrincingPage from './landing_page/Pricing/PricingPage';
+import Signup from './landing_page/Signup/Signup';
+import AboutPage from './landing_page/About/AboutPage';
+import SupportPage from './landing_page/Support/SupportPage';
+import ProductPage from './landing_page/Products/ProductPage';
+import Footer from './landing_page/Footer';
+import Navbar from './landing_page/Navbar';
+import NotFound from './landing_page/NotFount';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
- 
-  <HomePage />
-   </React.StrictMode>
+  <BrowserRouter>
+  <Navbar />
+  <Routes>
+    <Route path="/" element={<HomePage />}></Route>
+    <Route path="/Signup" element={<Signup />}></Route>
+     <Route path="/About" element={<AboutPage/>}></Route>
+      <Route path="/Products" element={<ProductPage />}></Route>
+       <Route path="/Pricing" element={<PrincingPage />}></Route>
+        <Route path="/Support" element={<SupportPage />}></Route>
+        <Route path="/*" element={<NotFound />}></Route>
+  </Routes>
+   <Footer/>
+  </BrowserRouter>
 );
 
 
--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
@@ -7,9 +7,9 @@ function Navbar() {
     <nav class="navbar navbar-expand-lg border-bottom"
       style={{ backgroundColor: "#FFF" }}>
       <div class="container p-2">
-        <a class="navbar-brand" href="#"><img src="media/images/logo.svg" alt="Logo"
+        <Link class="navbar-brand" to="/"><img src="media/images/logo.svg" alt="Logo"
           style={{ width: "25%" }}
-        /></a>
+        /></Link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -18,20 +18,20 @@ function Navbar() {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Signup</a>
+                <Link class="nav-link active" aria-current="page" to="/Signup">Signup</Link>
               </li>
               <li class="nav-item ">
-                <a class="nav-link active" href="#">About</a>
+                <Link class="nav-link active" to="/About">About</Link>
               </li>
               <li class="nav-item ">
-                <a class="nav-link active" href="#">Product</a>
+                <Link class="nav-link active" to="/Product">Product</Link>
               </li>
 
               <li class="nav-item ">
-                <a class="nav-link active" href="#">Pricing</a>
+                <Link class="nav-link active" to="/Pricing">Pricing</Link>
               </li>
               <li class="nav-item ">
-                <a class="nav-link active" href="#">Support</a>
+                <Link class="nav-link active" to="/Support">Support</Link>
               </li>
             </ul>
             </div>
