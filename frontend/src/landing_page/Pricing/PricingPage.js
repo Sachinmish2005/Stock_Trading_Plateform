f8f61a130d2bb2d5cf5ef95eb9b566a1bb4ca8a7
Delta student <sachin2005mishra@gmail.com>
2025-12-19 20:02:02 +0530

    Initial commit - Stock Trading Platform

---
+++ frontend/src/landing_page/Pricing/PricingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hero from './Hero';
+import Brokerage from './Brokerage';
+
+
+function PrincingPage() {
+  return ( 
+    <>
+    <Hero />
+    <Brokerage />
+    
+    </>
+   );
+}
+
+export default PrincingPage;
